6596aa131b4312bc9ab76726edfbdf3299244982
Sergiu Ser <mararassergiu@gmail.com>
2019-01-27 17:57:14 +0200

    add background, remove some div and classes not used.

--- src/App.js
+++ src/App.js
@@ -56,52 +56,47 @@ class App extends Component {
         <div className="row form-group">
           <Input />
         </div>
-        <div className="buttons justify-content-center">
-          <div className="row form-group">
-              <Button block outline color="secondary">(</Button>
-              <Button block outline color="secondary">(</Button>
-              <Button block outline color="secondary">%</Button>
-              <Button block outline color="secondary">AC</Button>
-          </div>
-          <div className="row">
-                <div className="col-9">
-                  <div className="row form-group">
-                    <Button block outline color="secondary">7</Button>
-                    <Button block outline color="secondary">8</Button>
-                    <Button block outline color="secondary">9</Button>
-                  </div>
-                  <div className="row form-group">
-                    <Button block outline color="secondary">4</Button>
-                    <Button block outline color="secondary">5</Button>
-                    <Button block outline color="secondary">6</Button>
-                  </div>
-                  <div className="row form-group">
-                      <Button block outline color="secondary">1</Button>
-                      <Button block outline color="secondary">2</Button>
-                      <Button block outline color="secondary">3</Button>
-                  </div>
+        <div className="row form-group">
+            <Button block outline color="secondary">(</Button>
+            <Button block outline color="secondary">(</Button>
+            <Button block outline color="secondary">%</Button>
+            <Button block outline color="secondary">AC</Button>
+        </div>
+        <div className="row">
+              <div className="col-9">
+                <div className="row form-group">
+                  <Button block outline color="secondary">7</Button>
+                  <Button block outline color="secondary">8</Button>
+                  <Button block outline color="secondary">9</Button>
                 </div>
-                <div className="col-3">
-                  <div className="row form-group">
-                
-                    <Button block outline color="secondary">/</Button>
-                   
-                  </div>
-                  <div className="row form-group">
-                    <Button block outline color="secondary">x</Button>
-                  </div>
-                  <div className="row form-group">
-                    <Button block outline color="secondary">-</Button>
-                  </div>
+                <div className="row form-group">
+                  <Button block outline color="secondary">4</Button>
+                  <Button block outline color="secondary">5</Button>
+                  <Button block outline color="secondary">6</Button>
                 </div>
-          </div>
-          <div className="row form-group">
-              <Button block outline color="secondary">0</Button>
-              <Button block outline color="secondary">.</Button>
-              <Button block outline color="primary">=</Button>
-              <Button block outline color="secondary">+</Button>
-          </div>
-            
+                <div className="row form-group">
+                    <Button block outline color="secondary">1</Button>
+                    <Button block outline color="secondary">2</Button>
+                    <Button block outline color="secondary">3</Button>
+                </div>
+              </div>
+              <div className="col-3">
+                <div className="row form-group">
+                  <Button block outline color="secondary">/</Button>
+                </div>
+                <div className="row form-group">
+                  <Button block outline color="secondary">x</Button>
+                </div>
+                <div className="row form-group">
+                  <Button block outline color="secondary">-</Button>
+                </div>
+              </div>
+        </div>
+        <div className="row">
+            <Button block outline color="secondary">0</Button>
+            <Button block outline color="secondary">.</Button>
+            <Button block outline color="primary">=</Button>
+            <Button block outline color="secondary">+</Button>
         </div>
       </div>
     );
